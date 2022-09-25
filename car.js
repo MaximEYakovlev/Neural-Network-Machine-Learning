bfdf03690f50702579165edb1b8265736fdc1a5e
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-25 12:00:00 +0300

    the car is moving

--- car.js
+++ car.js
@@ -8,6 +8,15 @@ class Car {
     this.controls = new Controls();
   }
 
+  update() {
+    if (this.controls.forward) {
+      this.y -= 2;
+    }
+    if (this.controls.reverse) {
+      this.y += 2;
+    }
+  }
+
   draw(ctx) {
     ctx.beginPath();
     ctx.rect(
