5dc6f2781c95a6f8032f82acf7581c4e13ba9809
Maxim Yakovlev <it.trainee@yandex.ru>
2022-09-27 12:00:00 +0300

    the speed limit is set

--- car.js
+++ car.js
@@ -7,6 +7,8 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
 
     this.controls = new Controls();
   }
@@ -18,6 +20,14 @@ class Car {
     if (this.controls.reverse) {
       this.speed -= this.acceleration;
     }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
     this.y -= this.speed;
   }
 
